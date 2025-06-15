4077bd315bd3d0c2fe50d2130cb1ad71d2d1a15b
Thien P. Dang <23521476@gm.uit.edu.vn>
2025-06-15 22:38:22 +0700

    Enhance InvoiceView and ViewModel functionality
    Updated InvoiceView.xaml to set a white background and added a TextBlock bound to the GhiChu property.
    Refactored InvoiceViewModel to inherit from BaseViewModel, introduced a GhiChu property with change notification, and initialized it in the constructor.

--- Presentation/ViewModel/InvoiceViewModel.cs
+++ Presentation/ViewModel/InvoiceViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyTiecCuoi.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,15 @@ using System.Threading.Tasks;
 
 namespace QuanLyTiecCuoi.Presentation.ViewModel
 {
-    public class InvoiceViewModel
+    public class InvoiceViewModel : BaseViewModel
     {
        private int _invoiceId;
+        private string _GhiChu;
+        public string GhiChu { get => _GhiChu; set { _GhiChu = value; OnPropertyChanged(); } }
         public InvoiceViewModel(int invoiceId)
         {
             _invoiceId = invoiceId;
+            GhiChu = invoiceId.ToString();
         }
     }
 }
